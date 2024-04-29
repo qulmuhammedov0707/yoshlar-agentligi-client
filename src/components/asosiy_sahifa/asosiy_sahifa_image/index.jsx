dd3aaad6cff9a8b81dc342704d8898e54a3b1c0d
qulmuhammedov <161331505+qulmuhammedov0707@users.noreply.github.com>
2024-04-29 16:13:31 +0500

    Buttons
    Router buttons set

--- src/components/asosiy_sahifa/asosiy_sahifa_image/index.jsx
+++ src/components/asosiy_sahifa/asosiy_sahifa_image/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import styles from './style.module.css'
+import { Link } from "react-router-dom";
 
 function AsosiySahifaImageComponent() {
    return(
       <>
-      <h1>AsosiySahifaImageComponent</h1>
+            <div className={styles.buttons}>
+         <Link to={'/biz-haqimizda'}><button className={styles.buttons__button}>Agentlik haqida</button></Link>
+         <Link to={'/maqsad-va-vazifalar'}><button className={styles.buttons__button}>Maqsad va vazifalar</button></Link>
+         <Link to={'/agentlik-tuzilmasi'}><button className={styles.buttons__button}>Agentlikning tuzilmasi</button></Link>
+         <Link to={'/rahbariyat'}><button className={styles.buttons__button}>Rahbariyat</button></Link>
+         <Link to={'/hududiy-boshqarmalar'}><button className={styles.buttons__button_this}>Hududiy boshqarmalar</button></Link>
+      </div>
       </>
    )
 }
